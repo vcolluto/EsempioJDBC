3da321d081fa1481572e86f7daee986247337aaa
Vincenzo Colluto <enzocoll@tiscali.it>
2024-05-27 13:01:44 +0200

    insert con dati parametrici

--- src/org/generation/italy/Main.java
+++ src/org/generation/italy/Main.java
@@ -3,12 +3,16 @@ package org.generation.italy;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
 
 public class Main {	
 
 	public static void main(String[] args) {
 		String url="jdbc:mysql://localhost:3306/magazzino";	//stringa di connessione (in questo caso per MySql, ma potrebbe essere diversa per altre tipologie di DBMS)
+		Scanner sc=new Scanner(System.in);
+		DateTimeFormatter df=DateTimeFormatter.ofPattern("dd/MM/yyyy");
 		
 		//significato parti della stringa:
 		// jdbc: Java Data Base Connectivity (API da utilizzare per connettersi al DB)
@@ -21,11 +25,43 @@ public class Main {
 		try (Connection conn=DriverManager.getConnection(url, "root", "jaita101")) {	//provo a connettermi
 			//la connessione è andata a buon fine
 			System.out.println("Connesso correttamente al database");
+			
+			System.out.println("*** INSERIMENTO MOVIMENTO ***");
+			
+			
+			//leggo i dati del movimento
+			System.out.print("Id: ");
+			int id=sc.nextInt();
+			sc.nextLine();
+			
+			System.out.print("Data: ");
+			LocalDate data=LocalDate.parse(sc.nextLine(),df);
+			
+			System.out.print("Cod prodotto: ");
+			String codProd=sc.nextLine();
+			
+			System.out.print("Cod movimento: ");
+			String codMov=sc.nextLine();
+			
+			System.out.print("Quantità: ");
+			int quantità=sc.nextInt();
+			sc.nextLine();
+			
+			
 			String sql="INSERT INTO movimenti(id, data, codProdotto, codMovimento, quantità) "
-					+ "VALUES(1, '2020-02-01', 'P01', 'E02', 12)";
+					+ "VALUES(?, ?, ?, ?, ?)";		//il ? indica un parametro
 			
 			System.out.println("Tentativo di esecuzione INSERT");
-			try (PreparedStatement ps=conn.prepareStatement(sql)) {		//provo a eseguire l'sql
+			try (PreparedStatement ps=conn.prepareStatement(sql)) {		//provo a creare l'istruzione sql
+				
+				//imposto i valori dei parametri				
+				ps.setInt(1, id);		//il primo parametro è l'id. NB: si parte dalla posizione 1
+				ps.setObject(2, data);	//il secondo parametro è la data
+				ps.setString(3, codProd);
+				ps.setString(4, codMov);
+				ps.setInt(5, quantità);
+				
+				
 				int righeInteressate=ps.executeUpdate();	//eseguo l'istruzione
 				System.out.println("Righe inserite: "+righeInteressate);
 			}
@@ -34,6 +70,7 @@ public class Main {
 			//si è verificato un problema. L'oggetto e (di tipo Exception) contiene informazioni sull'errore verificatosi
 			System.err.println("Si è verificato un errore: "+e.getMessage());
 		}
+		sc.close();
 	}
 
 }
